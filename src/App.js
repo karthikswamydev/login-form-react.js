470146c4ae2b4cd9b94112111b75713b3e8a7b58
karthik.swamydev <sudo.ks98@gmail.com>
2022-10-02 01:19:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import {useState} from "react"
 import './App.css';
+import LoginForm from "./components/LoginForm"
 
 function App() {
+  const adminUser = {
+    email: "karthik@gmail.com",
+    password:"mako@123"
+  }
+  const [user,setUser] = useState ({name:"",email:''})
+  const [error,setError] = useState("")
+
+  const Login = (details) =>{
+    if(details.email === adminUser.email &&  details.password === adminUser.password){
+      setUser({
+       name : details.name, 
+       email : details.email
+      })
+    console.log("user logged in")}else(
+      setError("Sorry wrong user credentials!!"))
+      console.log("invalid user")
+    console.log("details",details)
+  }
+
+  const Logout = () =>{
+    setUser({name:"",email:''})
+    console.log("logout")
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {(user.email !== "") ? (
+        <div className="welcome">
+          <h2>welcome,<span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+           </div>
+      ):(
+        <LoginForm Login={Login} error={error}></LoginForm>
+      )}
+      
     </div>
   );
 }
